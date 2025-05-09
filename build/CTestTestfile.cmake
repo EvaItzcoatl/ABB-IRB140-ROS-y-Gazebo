663d9881d4d6b1d84ae5d0409b394909b1f5259e
Eva <itzcoatl2902@gmail.com>
2025-05-09 16:45:03 -0600

    Primer commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eva/prueba1_ws/src
+# Build directory: /home/eva/prueba1_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("abb_irb140_support")
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/eva/prueba1_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
